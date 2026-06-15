a167728e39c18e221c550a7285e967b1e410a53d
Смотров Вячеслав <slavasmotrov42@gmail.com>
2026-06-15 21:19:49 +0300

    SSSSSSS

--- src/pages/CourseDetailPage.jsx
+++ src/pages/CourseDetailPage.jsx
@@ -6,7 +6,7 @@ import { useAuth } from '../contexts/AuthContext';
 import { GiDuration } from 'react-icons/gi';
 import { FaPlay, FaCheckCircle, FaClock, FaLeaf, FaStar, FaFire, FaAward } from 'react-icons/fa';
 
-const API_URL = `http://${import.meta.env.VITE_API_URL}`;
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 const CourseDetailPage = () => {
   const { id } = useParams();
