dcba428429916e59af543e74452159a4115400bb
Смотров Вячеслав <slavasmotrov42@gmail.com>
2026-06-15 17:25:07 +0300

    WW

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,12 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = process.env.SECRET_KEY;
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) return res.status(401).json({ error: 'Доступ запрещен' });
   
   try {
-    const decoded = jwt.verify(token, SECRET_KEY);
-    req.user = decoded; // теперь содержит { userId, username, role }
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
     next();
   } catch (error) {
     res.status(401).json({ error: 'Неверный токен' });
