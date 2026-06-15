0b9c3ac7d78f8b4493653d882d4cab308d4a8b73
Смотров Вячеслав <slavasmotrov42@gmail.com>
2026-06-15 20:35:47 +0300

    SSS3

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'https://chmpnsgetto-meditation-app-5f61.twc1.net';
+const API_URL = 'http://chmpnsgetto-meditation-app-6dfa.twc1.net';
 const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
