92da5a6f9117e45cbd2c97d2c195e68aa3122c6e
Смотров Вячеслав <slavasmotrov42@gmail.com>
2026-06-15 21:08:40 +0300

    S2

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = `http://${import.meta.env.VITE_API_URL}`;
 const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
